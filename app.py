324cb7eac865aa5ca92581884dc40b895faeb66e
kamil <kamilanugrah2@gmail.com>
2024-01-18 22:18:35 +0700

    isi baru template

--- app.py
+++ app.py
@@ -9,4 +9,12 @@ def index():
 
 @app.route('/produk')
 def produk():
-    return render_template('produk.html')
+    return render_template('frontend/produk.html')
+
+@app.route('/detail-produk')
+def detail():
+    return render_template('frontend/detail.html')
+
+@app.route('/dashboards')
+def dashboards():
+    return render_template('frontend/dashboards.html')
