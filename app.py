fd489c2e047beb9c6100a676e97a6dd7f57bc5db
kamil <kamilanugrah2@gmail.com>
2024-01-18 20:05:05 +0700

    updat template

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/produk')
+def produk():
+    return render_template('produk.html')
